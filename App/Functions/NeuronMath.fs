959d40547a011fe22d4515b8c88c8c8576570895
Ricardo Milan <ricardo@fluencypass.com>
2022-06-05 16:18:59 -0300

    calculando 2 layers

--- App/Functions/NeuronMath.fs
+++ App/Functions/NeuronMath.fs
@@ -1,23 +1,21 @@
 ﻿namespace Functions
 
-module NeuronMath = 
-    open Types.NeuronTypes
-
-    let dotWithBias (dotProducts: DotProduct seq) (bias : Bias seq) = 
+module NeuronMath =
+    let dotWithBias (dotProducts: float seq) (bias : float seq) : float seq = 
         Seq.map2 (+) dotProducts bias 
         
-    let private dotWeightsByInputs (weight : Weights) (input : Inputs) : DotProduct = Seq.fold2 (fun state i w -> state + i * w) LanguagePrimitives.GenericZero input weight
+    let private dotWeightsByInputs (weight : float seq) (input : float seq) : float = Seq.fold2 (fun state i w -> state + i * w) LanguagePrimitives.GenericZero input weight
 
-    let private dotWeitghtSeqByInputs (weightSequence : Weights seq) (inputs : Inputs) =
+    let private dotWeitghtSeqByInputs (weightSequence : float seq seq) (inputs : float seq) =
         weightSequence |> Seq.map(fun weights -> dotWeightsByInputs weights inputs)
 
-    let private dotWeightSeqByInputSeq (weightSequence : Weights seq) (inputs : Inputs seq) =
+    let private dotWeightSeqByInputSeq (weightSequence : float seq seq) (inputs : float seq seq) =
         Seq.map (fun i -> dotWeitghtSeqByInputs weightSequence i) inputs
 
     let dot<'T> (w:obj) (i:obj)= 
         match (w, i) with 
-        | (:? Weights as w0), (:? Inputs as i0) -> dotWeightsByInputs w0 i0 |> box
-        | (:? seq<Weights> as w1), (:? Inputs as i1) -> dotWeitghtSeqByInputs w1 i1 |> box
-        | (:? seq<Weights> as w2), (:? seq<Inputs> as i2) -> dotWeightSeqByInputSeq w2 i2 |> box
+        | (:? seq<seq<float>> as w2), (:? seq<seq<float>> as i2) -> dotWeightSeqByInputSeq w2 i2 |> box
+        | (:? seq<seq<float>> as w1), (:? seq<float> as i1) -> dotWeitghtSeqByInputs w1 i1 |> box
+        | (:? seq<float> as w0), (:? seq<float> as i0) -> dotWeightsByInputs w0 i0 |> box
         | _ -> None |> box
         :?> 'T
--- App/Program.fs
+++ App/Program.fs
@@ -1,28 +1,40 @@
-﻿open Types.NeuronTypes
-open Functions.NeuronMath
+﻿open Functions.NeuronMath
 
 printfn "=============================="
 printfn "NEURAL NETWORK FROM SCRATCH!!!"
 
-let inputs : Inputs seq = [|
+let inputs : seq<float> seq = [|
         [| 1.0; 2.0; 3.0; 2.5 |];
         [|2.0; 5.0; -1.0; 2.0|];
         [|-1.5; 2.7; 3.3; -0.8|]
     |]
 
-let weightsSeq : Weights seq = [|
+let weightsSeq : seq<float> seq = [|
         [|0.2; 0.8; -0.5; 1.0|];
         [|0.5; -0.91; 0.26; -0.5|];
         [|-0.26; -0.27; 0.17; 0.87|]
     |]
 
-let bias : Bias seq = [| 2.0; 3.0; 0.5 |]
+let bias : seq<float> = [| 2.0; 3.0; 0.5 |]
 
-let dotProducts = dot<DotProduct seq seq> weightsSeq inputs
 
-printfn "RESULTADO DA CAMADA!!!"
-for dotProduct in dotProducts do
-    Seq.iter (printf "\t%A") (dotWithBias dotProduct bias)
+let weightsSeq2 : seq<float> seq = [|
+        [|0.1; -0.14; 0.5;|];
+        [|-0.5; 0.12; -0.33;|];
+        [|-0.44; 0.73; -0.13;|]
+    |]
+
+let bias2 : seq<float> = [| -1.0; 2.0; -0.5 |]
+
+let result = dot<float seq seq> weightsSeq inputs
+                |> Seq.map(fun f -> dotWithBias f bias) // Resultado da camada 1
+                |> dot<float seq seq> weightsSeq2
+                |> Seq.map(fun f -> dotWithBias f bias2) // Resultado da camada 2
+
+for a in result do 
+    for b in a do
+        printf "%A\t" b
     printf "\n"
+    
 
 System.Console.ReadKey() |> ignore
